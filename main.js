88f16a582fe1d0d41e5f8fa02016b1e53b091033
Maks Bessonov <max8essonov@gmail.com>
2025-04-02 12:11:04 +0500

    refactor playRound function to enable async delays and remove commented-out code

--- main.js
+++ main.js
@@ -82,9 +82,9 @@ function playGame() {
 
   async function playRound(humanChoice, computerChoice) {
     createNewDiv(`You chose ${humanChoice}`);
-    /* await asyncTimeout(1000); */
+    await asyncTimeout(1000);
     createNewDiv(`Computer chose ${computerChoice}`);
-    /* await asyncTimeout(1000); */
+    await asyncTimeout(1000);
     // tie condition
     if (humanChoice === computerChoice) {
       ++functionCalls;
@@ -124,61 +124,3 @@ function playGame() {
   }
   playRound(...arguments);
 }
-/*  My shitty variant
-function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function getHumanChoice() {
-  return prompt();
-}
-
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    const humanSelection = humanChoice.toLowerCase();
-    if (
-      humanSelection !== "rock" &&
-      humanSelection !== "paper" &&
-      humanSelection !== "scissors"
-    ) {
-      throw new Error('You should type "rock", "paper" or "scissors"');
-    }
-    if (humanSelection === computerChoice) {
-      return "It's a tie! Try again!";
-    } else if (humanSelection === "paper" && computerChoice === "scissors") {
-      ++computerScore;
-      return "You lose! Scissors beat paper.";
-    } else if (humanSelection === "paper" && computerChoice === "rock") {
-      ++humanScore;
-      return "You win! Paper beats rock.";
-    } else if (humanSelection === "scissors" && computerChoice === "paper") {
-      ++humanScore;
-      return "You win! Scissors beat paper.";
-    } else if (humanSelection === "scissors" && computerChoice === "rock") {
-      ++computerScore;
-      return "You lose! Rock beats scissors.";
-    } else if (humanSelection === "rock" && computerChoice === "paper") {
-      ++computerScore;
-      return "You lose! Paper beats rock.";
-    } else if (humanSelection === "rock" && computerChoice === "scissors") {
-      ++humanScore;
-      return "You win! Rock beat scissors.";
-    }
-  }
-  do {
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
-  } while (humanScore !== 5 && computerScore !== 5);
-  if (humanScore === 5) {
-    console.log("Congatulations! You won!");
-  } else {
-    console.log("Sorry! You lose!");
-  }
-}
-
-playGame();
-
-*/
